c89ef7c2664cbcab8a6b246cbe0e6d9a2c5b15cc
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-05-10 20:18:06 +0700

    implement useFormState for validation in meals create data

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to create meals data, please try again later.</p>
+    </main>
+  );
+}
